83d120ca62504123ca0d878baaf79fe778640ded
nuruslaily <nuruslaily88@gmail.com>
2020-10-20 19:11:44 +0700

    update

--- app/src/main/java/id/ac/polinema/skor/fragments/GoalFragment.java
+++ app/src/main/java/id/ac/polinema/skor/fragments/GoalFragment.java
@@ -1,21 +1,24 @@
 package id.ac.polinema.skor.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import id.ac.polinema.skor.R;
+import id.ac.polinema.skor.databinding.FragmentGoalBinding;
 import id.ac.polinema.skor.models.GoalScorer;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class GoalFragment extends Fragment {
 
 	private String requestKey;
 	private GoalScorer goalScorer;
+
 
 	public GoalFragment() {
 		// Required empty public constructor
@@ -30,14 +33,36 @@ public class GoalFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
-		return null;
+		FragmentGoalBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_goal, container, false);
+		View view = binding.getRoot();
+		final GoalScorer goalScorer = requireArguments().getParcelable("GoalScorer");
+
+		binding.buttonSave.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				onSaveClicked(view);
+			}
+		});
+		binding.buttonCancel.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				onCancelClicked(view);
+			}
+		});
+		return view;
+
 	}
 
 	public void onSaveClicked(View view) {
+		Bundle bundle = new Bundle();
+		bundle.putParcelable("GoalScorer", goalScorer);
 
+		Navigation.findNavController(view).navigate(R.id.goal_scorer_action, bundle);
 	}
 
 	public void onCancelClicked(View view) {
-
+		Bundle bundle = new Bundle();
+		bundle.putParcelable("GoalScorer", goalScorer);
+		Navigation.findNavController(view).navigate(R.id.goal_scorer_action, bundle);
 	}
 }
--- app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
+++ app/src/main/java/id/ac/polinema/skor/fragments/ScoreFragment.java
@@ -5,48 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import id.ac.polinema.skor.models.GoalScorer;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ScoreFragment extends Fragment {
-
-	public static final String HOME_REQUEST_KEY = "home";
-	public static final String AWAY_REQUEST_KEY = "away";
-	public static final String SCORER_KEY = "scorer";
-
-	private List<GoalScorer> homeGoalScorerList;
-	private List<GoalScorer> awayGoalScorerList;
-
-	public ScoreFragment() {
-		// Required empty public constructor
-	}
+	GoalScorer goalScorer;
+	public static final String HOME_REQUEST_KEY = "home_requestKey";
+	public static final String AWAY_REQUEST_KEY = "away_requestKey";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.homeGoalScorerList = new ArrayList<>();
-		this.awayGoalScorerList = new ArrayList<>();
+
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
+
 		return null;
 	}
-
 	public void onAddHomeClick(View view) {
 
+		ScoreFragmentDirections.GoalScorerAction action = ScoreFragmentDirections.goalScorerAction(HOME_REQUEST_KEY);
+		Navigation.findNavController(view).navigate(action);
 	}
 
 	public void onAddAwayClick(View view) {
-
+		ScoreFragmentDirections.GoalScorerAction action = ScoreFragmentDirections.goalScorerAction(AWAY_REQUEST_KEY);
+		Navigation.findNavController(view).navigate(action);
 	}
 
 }
